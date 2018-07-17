26b24722fdc51dd40435c9e0cb0774265765dc37
simone bignotti <simone.bignotti@thinkopen.it>
2018-07-17 18:02:26 +0200

    Add POST /users/login

--- server/server.js
+++ server/server.js
@@ -115,4 +115,16 @@ app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredential(body.email, body.password).then((user) => {
+    user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 module.exports = {app};
